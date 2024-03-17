d4a5aebe110c15c84bcb95a90926dd75ed101059
ARFA79 <arfa79lg@gmail.com>
2024-03-17 05:59:59 +0330

    create project , app and models

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    email = models.EmailField()
