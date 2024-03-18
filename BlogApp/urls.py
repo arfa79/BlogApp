29483727e21c3245460fd0f1c81f2b21f40452f6
ARFA79 <arfa79lg@gmail.com>
2024-03-18 09:55:48 +0330

    views problem fixed

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from views import PostViewSet
+from blog.views import PostViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
